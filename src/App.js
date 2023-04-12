6b1433bd5ec57b35626a3a755f1ae8ab101096c8
bujosa <davidbujosa@gmail.com>
2023-04-11 20:27:01 -0400

    feat: create a new navbar

--- src/App.js
+++ src/App.js
@@ -1,86 +1,59 @@
 import React, { useState } from 'react';
 import './css/App.css';
-import {
-  Navbar,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  TabContent,
-  TabPane,
-  Row,
-  Col,
-} from 'reactstrap';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, Tab, Tabs } from '@material-ui/core';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
 import WorkHistory from './components/WorkHistory';
 import Education from './components/Education';
 
-function App() {
-  const [activeTab, setActiveTab] = useState('1');
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  appBar: {
+    color: '#333',
+    backgroundColor: 'white',
+  },
+  toolBar: {
+    justifyContent: 'space-between',
+  },
+  tabs: {
+    marginRight: theme.spacing(3),
+  },
+  contentContainer: {
+    marginTop: '64px',
+  },
+}));
 
-  const toggle = (tab) => {
-    if (activeTab !== tab) setActiveTab(tab);
+function App() {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
 
   return (
-    <div className="app-container">
-      <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">Home</NavbarBrand>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink
-              href="#projects"
-              onClick={() => {
-                toggle('3');
-              }}>
-              Projects
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              href="#work-history"
-              onClick={() => {
-                toggle('2');
-              }}>
-              Work History
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              href="#education"
-              onClick={() => {
-                toggle('4');
-              }}>
-              Education
-            </NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
-
-      <div className="content-container">
-        <TabContent activeTab={activeTab}>
-          <TabPane tabId="1">
-            <Row>
-              <Col sm="12">
-                <h4>Welcome to my personal website!</h4>
-                <p>
-                  Here you can find information about my work history, projects,
-                  and education.
-                </p>
-              </Col>
-            </Row>
-          </TabPane>
-          <TabPane tabId="2">
-            <WorkHistory />
-          </TabPane>
-          <TabPane tabId="3">
-            <Projects />
-          </TabPane>
-          <TabPane tabId="4">
-            <Education />
-          </TabPane>
-        </TabContent>
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appBar} color="inherit">
+        <Toolbar className={classes.toolBar}>
+          <h4>Welcome to my personal website!</h4>
+          <Tabs
+            className={classes.tabs}
+            value={value}
+            onChange={handleChange}
+            aria-label="Navigation Tabs">
+            <Tab label="Work History" />
+            <Tab label="Projects" />
+            <Tab label="Education" />
+          </Tabs>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.contentContainer}>
+        {value === 0 && <WorkHistory />}
+        {value === 1 && <Projects />}
+        {value === 2 && <Education />}
         <Contact />
       </div>
     </div>
