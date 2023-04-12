163889d841c7d3bf34dd8647466860e861f7bbef
bujosa <davidbujosa@gmail.com>
2023-04-11 20:43:29 -0400

    style: add second approach

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,22 +1,54 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import {
-  Card,
-  CardContent,
-  CardMedia,
-  Typography,
-  Chip,
   Grid,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  Typography,
+  IconButton,
 } from '@material-ui/core';
-import projectData from './data/projects';
+import { makeStyles } from '@material-ui/core/styles';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+import projectsData from './data/projects';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    padding: theme.spacing(4),
+  },
+  card: {
+    maxWidth: 345,
     margin: theme.spacing(2),
+    borderRadius: theme.spacing(1),
+    overflow: 'hidden',
+    boxShadow: theme.shadows[4],
+    transition: 'transform 0.2s',
+    '&:hover': {
+      transform: 'scale(1.05)',
+    },
   },
   media: {
-    height: 140,
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  content: {
+    padding: theme.spacing(2),
+  },
+  tags: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginTop: theme.spacing(2),
+  },
+  tag: {
+    margin: theme.spacing(0.5),
+    padding: theme.spacing(0.5, 1),
+    backgroundColor: theme.palette.grey[300],
+    borderRadius: theme.spacing(0.5),
+  },
+  github: {
+    marginLeft: 'auto',
   },
 }));
 
@@ -25,38 +57,37 @@ const Projects = () => {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={2}>
-        {projectData.map((project) => (
-          <Grid item xs={12} sm={6} md={4} key={project.id}>
-            <Card>
+      <Grid container justify="center" alignItems="center">
+        {projectsData.map((project) => (
+          <Grid item key={project.id}>
+            <Card className={classes.card}>
+              <CardHeader title={project.title} subheader={project.category} />
               <CardMedia
                 className={classes.media}
                 image={project.image}
                 title={project.title}
               />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {project.title}
-                </Typography>
+              <CardContent className={classes.content}>
                 <Typography variant="body2" color="textSecondary" component="p">
                   {project.description}
                 </Typography>
-                <br />
-                <div>
-                  {project.tags.map((tag, index) => (
-                    <Chip
-                      key={index}
-                      size="small"
-                      label={tag}
-                      style={{ marginRight: 5, marginBottom: 5 }}
-                    />
+                <div className={classes.tags}>
+                  {project.tags.map((tag) => (
+                    <Typography
+                      variant="body2"
+                      key={tag}
+                      className={classes.tag}>
+                      {tag}
+                    </Typography>
                   ))}
                 </div>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  <strong>Category: </strong>
-                  {project.category}
-                </Typography>
               </CardContent>
+              <IconButton
+                aria-label="github"
+                className={classes.github}
+                onClick={() => window.open(project.github)}>
+                <GitHubIcon />
+              </IconButton>
             </Card>
           </Grid>
         ))}
--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -1,39 +1,63 @@
 const projectData = [
   {
     id: 1,
-    title: 'Solidity Smart Contract',
+    title: 'Project 1',
+    category: 'Solidity',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nulla ac risus pellentesque, sit amet consequat lectus vestibulum.',
-    tags: ['Solidity', 'Ethereum', 'Blockchain'],
-    category: 'Blockchain',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis imperdiet leo. Nulla a eros et tellus aliquam tincidunt. Duis a arcu est. Suspendisse vitae fringilla lectus. ',
     image: 'https://picsum.photos/400/300?random=1',
+    github: 'https://github.com/example-project-1',
+    tags: ['Solidity', 'Blockchain', 'Ethereum'],
   },
   {
     id: 2,
-    title: 'Flutter Mobile App',
+    title: 'Project 2',
+    category: 'Flutter',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nulla ac risus pellentesque, sit amet consequat lectus vestibulum.',
-    tags: ['Flutter', 'Mobile', 'UI'],
-    category: 'Mobile',
-    image: 'https://picsum.photos/400/300?random=2',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis imperdiet leo. Nulla a eros et tellus aliquam tincidunt. Duis a arcu est. Suspendisse vitae fringilla lectus. ',
+    image: 'https://picsum.photos/400/300?random=6',
+    github: null,
+    tags: ['Flutter', 'Mobile App'],
   },
   {
     id: 3,
-    title: 'Node.js REST API',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nulla ac risus pellentesque, sit amet consequat lectus vestibulum.',
-    tags: ['Node.js', 'Express', 'REST API'],
+    title: 'Project 3',
     category: 'Backend',
-    image: 'https://picsum.photos/400/300?random=3',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis imperdiet leo. Nulla a eros et tellus aliquam tincidunt. Duis a arcu est. Suspendisse vitae fringilla lectus. ',
+    image: 'https://picsum.photos/400/300?random=5',
+    github: 'https://github.com/example-project-3',
+    tags: ['Backend', 'API'],
   },
   {
     id: 4,
-    title: 'React Dashboard',
+    title: 'Project 4',
+    category: 'Flutter',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod nulla ac risus pellentesque, sit amet consequat lectus vestibulum.',
-    tags: ['React', 'Dashboard', 'UI'],
-    category: 'Frontend',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis imperdiet leo. Nulla a eros et tellus aliquam tincidunt. Duis a arcu est. Suspendisse vitae fringilla lectus. ',
     image: 'https://picsum.photos/400/300?random=4',
+    github: null,
+    tags: ['Flutter', 'Mobile App'],
+  },
+  {
+    id: 5,
+    title: 'Project 5',
+    category: 'Backend',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis imperdiet leo. Nulla a eros et tellus aliquam tincidunt. Duis a arcu est. Suspendisse vitae fringilla lectus. ',
+    image: 'https://picsum.photos/400/300?random=3',
+    github: null,
+    tags: ['Backend', 'Database'],
+  },
+  {
+    id: 6,
+    title: 'Project 6',
+    category: 'Solidity',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis imperdiet leo. Nulla a eros et tellus aliquam tincidunt. Duis a arcu est. Suspendisse vitae fringilla lectus. ',
+    image: 'https://picsum.photos/400/300?random=2',
+    github: 'https://github.com/example-project-6',
+    tags: ['Solidity', 'Blockchain', 'Ethereum'],
   },
 ];
 
