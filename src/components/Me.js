7f7cafcb5faef2fb8c75f19fbfa61658543a1561
bujosa <davidbujosa@gmail.com>
2023-04-12 20:04:16 -0400

    feat: create me component

---
+++ src/components/Me.js
@@ -0,0 +1,71 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Button, Box } from '@material-ui/core';
+import { Computer as ComputerIcon } from '@material-ui/icons';
+import GetAppIcon from '@material-ui/icons/GetApp';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: theme.spacing(3),
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+  icon: {
+    fontSize: 72,
+    color: theme.palette.secondary.main,
+    marginBottom: theme.spacing(2),
+  },
+  button: {
+    marginTop: theme.spacing(2),
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.secondary.contrastText,
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.dark,
+    },
+  },
+  title: {
+    marginBottom: theme.spacing(2),
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    marginBottom: theme.spacing(2),
+    fontStyle: 'italic',
+  },
+}));
+
+const Me = () => {
+  const classes = useStyles();
+
+  return (
+    <Box className={classes.root}>
+      <ComputerIcon className={classes.icon} />
+      <Typography
+        variant="h3"
+        component="h2"
+        align="center"
+        className={classes.title}>
+        Hi, I'm David Bujosa
+      </Typography>
+      <Typography variant="h5" align="center" className={classes.subtitle}>
+        Web Developer &amp; Software Engineer
+      </Typography>
+      <Typography variant="body1" align="center">
+        I'm passionate about building software that helps people solve
+        real-world problems.
+      </Typography>
+      <Button
+        variant="contained"
+        className={classes.button}
+        startIcon={<GetAppIcon />}
+        href="../cv/resume.pdf"
+        download>
+        Download My Resume
+      </Button>
+    </Box>
+  );
+};
+
+export default Me;
