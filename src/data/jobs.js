3a1482f7dd66c6ed90d12987c21887fdb401b9cf
bujosa <davidbujosa@gmail.com>
2023-04-13 16:25:20 -0400

    feat: add jobs data

---
+++ src/data/jobs.js
@@ -0,0 +1,25 @@
+import karvi from './karvi.png';
+import curbo from './curbo.png';
+
+const jobData = [
+  {
+    id: 1,
+    company: 'Karvi',
+    date: '2023 - present',
+    description: 'A platform for buying and selling vehicles. (karvi.com)',
+    details:
+      'Developed and maintained the backend of the platform using NestJS, MongoDB, and Python.',
+    image: karvi,
+  },
+  {
+    id: 2,
+    company: 'Curbo Technologies',
+    date: '2020 - 2023',
+    description: 'A platform for buying and selling vehicles. (curbo.com)',
+    details:
+      'Developed and maintained the backend of the platform using NestJS, MongoDB, and GCP. The architecture of the platform is microservice-based and is deployed on GCP. I also maintained and structure the database of the platform.',
+    image: curbo,
+  },
+];
+
+export default jobData;
